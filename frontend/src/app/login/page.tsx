42921b488358da46d2997fb8682889c059256c7d
24LP2840 24LP2840 <24LP2840@G301-CC0193.local>
2025-04-22 12:41:49 +0800

    login sign setup

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import WorldMap from '@/components/ui/world-map';
+import { WorldMapDemo } from './_components/worldmap';
+
+const LoginPage = () => {
+  return <div>
+    <div className='h-[500px] w-[1275px] flex items-center justify-center'>
+    <WorldMapDemo/>
+    </div>
+  </div>;
+};
+
+export default LoginPage;
