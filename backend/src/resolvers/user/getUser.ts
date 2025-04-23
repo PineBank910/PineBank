ea2d40c9692fda080ef0fb4d78d6758373ecbffd
24LP2417 24LP2417 <24LP2417@G301-CC0203-2.local>
2025-04-23 13:09:24 +0800

    BACKEND:get user data

---
+++ backend/src/resolvers/user/getUser.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const getUser = async (req: Request, res: Response): Promise<any> => {
+  try {
+    const userId = req.user?.id;
+
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+        include: {
+            userProfile: true,
+        },
+    });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json({ message: "User data fetched successfully", user });
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .json({ message: "Error occurred while fetching user data" });
+  }
+};
--- backend/src/routers/userRouter.ts
+++ backend/src/routers/userRouter.ts
@@ -1,15 +1,11 @@
 import express from "express";
 import { createUser } from "../resolvers/user/createUser";
 import { login } from "../resolvers/user/loginUser";
-// import { authorizationMiddleware } from "../middlewares/authorization";
-// import { createUser } from "../resolvers/users/createUser";
-// import { login } from "../resolvers/users/login";
-// import { getUser } from "../resolvers/users/getUser";
-// import { createProfile } from "../resolvers/users/createProfile";
+import { authorizationMiddleware } from "../middlewares/authorization";
+import { getUser } from "../resolvers/user/getUser";
 
 export const userRouter = express.Router();
 
 userRouter.post("/signup", createUser);
 userRouter.post("/login", login);
-// userRouter.get("/", authorizationMiddleware, getUser);
-// userRouter.post("/profile", authorizationMiddleware, createProfile);
+userRouter.get("/", authorizationMiddleware, getUser);
