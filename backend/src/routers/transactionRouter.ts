bc578fcbc60bf241f9fe8cfd5b0a6b6a820857fc
24LP9558 24LP9558 <24LP9558@G301-CC0194.local>
2025-04-24 10:27:30 +0800

    BACKEND: transaction router added

---
+++ backend/src/routers/transactionRouter.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import { createTransaction } from "../resolvers/transaction/createTransaction";
+import { getAccountIncomeOutcome } from "../resolvers/transaction/accountTransaction";
+
+export const transactionRouter = express.Router(); 
+
+transactionRouter.post("/", createTransaction);
+transactionRouter.get("/:accountId/income-outcome", getAccountIncomeOutcome);
+
