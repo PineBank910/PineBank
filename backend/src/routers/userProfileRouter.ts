9254491df37c73ed3e4fbfc55623cef44525e85f
24LP2417 24LP2417 <24LP2417@G301-CC0203-2.local>
2025-04-24 12:50:49 +0800

    BACKEND: add zod to profile

--- backend/src/routers/userProfileRouter.ts
+++ backend/src/routers/userProfileRouter.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import { viewProfile } from "../resolvers/userProfile/getProfile";
 import { createProfile } from "../resolvers/userProfile/createProfile";
+import { validate } from "../middlewares/validate";
+import { userProfileSchema } from "../validators/profileSchema";
+import { authorizationMiddleware } from "../middlewares/authorization";
 
 export const userProfileRouter = express.Router();
 
-userProfileRouter.post("/", createProfile);
-userProfileRouter.get("/", viewProfile);
+userProfileRouter.post("/", authorizationMiddleware, validate(userProfileSchema),createProfile);
+userProfileRouter.get("/", authorizationMiddleware, viewProfile);
