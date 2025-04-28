5b26177c22189e4046340bd1cafabe21642c4138
24LP9558 24LP9558 <24LP9558@G301-CC0194.local>
2025-04-28 12:50:09 +0800

    BACKEND: few fix

--- backend/src/resolvers/user/getUser.ts
+++ backend/src/resolvers/user/getUser.ts
@@ -1,22 +1,25 @@
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
+import { clerkClient, getAuth } from "@clerk/express";
 
 const prisma = new PrismaClient();
 
 export const getUser = async (req: Request, res: Response): Promise<any> => {
   try {
-    const userId = req.user?.id;
-    console.log("User ID from request:", userId); // Log the userId to check if it's coming through
+
+    const { userId } = getAuth(req);
 
     if (!userId) {
-      console.log("User ID is missing"); // Log if there's no userId
-      return res
-        .status(400)
-        .json({ message: "User ID is missing from the token" });
+      console.log("User ID is missing");
+      res.status(400).json({ message: "User ID is missing from the token" });
+      return
     }
 
+    const clerkUser = await clerkClient.users.getUser(userId);
+    console.log("User ID from request:", userId);
+
     const user = await prisma.user.findUnique({
-      where: { id: userId },
+      where: { clerkId: clerkUser.id },
       include: {
         userProfile: true,
         loans: true,
@@ -25,16 +28,14 @@ export const getUser = async (req: Request, res: Response): Promise<any> => {
     });
 
     if (!user) {
-      console.log("User not found"); // Log if user is not found
-      return res.status(404).json({ message: "User not found" });
+      console.log("User not found");
+       res.status(404).json({ message: "User not found" });
     }
 
-    console.log("User found:", user); // Log the user data
-    return res.json({ message: "User data fetched successfully", user });
+    console.log("User found:", user);
+     res.json({ message: "User data fetched successfully", user });
   } catch (err) {
-    console.error("Error fetching user:", err); // Log error details
-    return res
-      .status(500)
-      .json({ message: "Error occurred while fetching user data" });
+    console.error("Error fetching user:", err);
+     res.status(500).json({ message: "Error occurred while fetching user data" });
   }
 };
--- backend/src/routers/userRouter.ts
+++ backend/src/routers/userRouter.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import { authorizationMiddleware } from "../middlewares/authorization";
 import { getUser } from "../resolvers/user/getUser";
 import { CreateUserV2 } from "../resolvers/user/createUserV2";
+import { requireAuth } from "@clerk/express";
 
 
 export const userRouter = express.Router();
 
 userRouter.post("/signup", CreateUserV2);
-userRouter.get("/", authorizationMiddleware, getUser);
+userRouter.get("/",requireAuth(), getUser);
