3e1414502967cb13dbc23f738f91ca906df35aad
24LP0058 24LP0058 <24LP0058@G301-CC0203.local>
2025-04-30 12:45:26 +0800

    FRONT: Dashboardiin l neg ym

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -5,25 +5,18 @@ const Dashboard = () => {
     <>
       <div className="pl-[25px] lg:pl-[40px] mt-6 text-[#343C6A] dark:text-[white] flex flex-wrap gap-7   ">
         <div>
+          <p className="font-semibold text-lg">Миний данс </p>
+          <div className="w-[300px] h-[150px] border">
+            <div className="text-center">9911223344</div>
+          </div>
+        </div>
+        <div>
           <p className="font-semibold text-lg">My cards</p>
           <div className="mt-5 flex gap-7 ">
             {/* card geed component gargii */}
-            <div className=" w-[265px] h-[170px] lg:w-[350px] lg:h-[235px] bg-amber-300 rounded-[15px]">
-              text
+            <div className=" w-[265px] h-[170px] lg:w-[350px] lg:h-[235px] border rounded-[15px] flex justify-center items-center cursor-pointer">
+              + Create a card
             </div>
-            <div className=" w-[265px] h-[170px] lg:w-[350px] lg:h-[235px] bg-amber-300 rounded-[15px]">
-              text
-            </div>
-          </div>
-        </div>
-        <div className="min-w-3xs ">
-          {/* {Transaction component gargii} */}
-          <p className="font-semibold text-lg">Recent transactions</p>
-          <div className="w-full rounded-2xl bg-white dark:bg-gray-800 ">
-            <div className="w-full h-12  ">Some transactions here</div>
-            <div className="w-full h-12  ">Some transactions here</div>
-            <div className="w-full h-12  ">Some transactions here</div>
-            <div className="w-full h-12  ">Some transactions here</div>
           </div>
         </div>
       </div>
