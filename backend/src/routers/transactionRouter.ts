89bd232b7696cf1aece5912939f7084600bad6f9
Gantulga0 <tudewwdagwagantulga@gmail.com>
2025-05-07 00:04:03 +0800

    BACKEND: gettransaction and router index.ts upadte

--- backend/src/routers/transactionRouter.ts
+++ backend/src/routers/transactionRouter.ts
@@ -1,11 +1,10 @@
 import express from "express";
 import { createTransaction } from "../resolvers/transaction/createTransaction";
 import { getAccountIncomeOutcome } from "../resolvers/transaction/accountTransaction";
+import { getTransaction } from "../resolvers/transaction/getUsersTransaction";
 
-export const transactionRouter = express.Router(); 
+export const transactionRouter = express.Router();
 
-transactionRouter.post("/",  createTransaction);
-transactionRouter.get("/:accountId/income-outcome",  getAccountIncomeOutcome);
-
-
-
+transactionRouter.post("/", createTransaction);
+transactionRouter.get("/:accountId/income-outcome", getAccountIncomeOutcome);
+transactionRouter.get("/", getTransaction);
