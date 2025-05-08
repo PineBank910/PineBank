cfefe633f50e942afdaca95737baf19b8b8d39b5
24LP9558 24LP9558 <24LP9558@G301-CC0194.local>
2025-05-08 12:05:33 +0800

    FRONTEND: dashboard fix

---
+++ frontend/src/app/dashboard/_components/AccountSelector.tsx
@@ -0,0 +1,103 @@
+import React, { useContext, useEffect } from "react";
+
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+} from "@/components/ui/select";
+import { CurrentUser } from "@/lib/currentUserContext";
+import { formatNumber } from "@/utils/balanceFormat";
+
+type ChooseAccountProps = {
+    selectedAccountId: string;
+    setSelectedAccountId: (accountId: string) => void;
+};
+const AccountSelector = (props: ChooseAccountProps) => {
+    const context = useContext(CurrentUser);
+    const currentUserData = context?.currentUserData;
+    const { selectedAccountId, setSelectedAccountId } = props;
+
+    useEffect(() => {
+        if (
+            currentUserData &&
+            Array.isArray(currentUserData.accounts) &&
+            currentUserData.accounts.length > 0
+        ) {
+            setSelectedAccountId(currentUserData.accounts[0].id);
+        }
+    }, [currentUserData]);
+
+    if (!context || !context.currentUserData) {
+        return <div>...Loading</div>;
+    }
+
+    if (!currentUserData) {
+        return <div>...Loading</div>;
+    }
+
+    const { accounts } = currentUserData;
+
+    if (!accounts || accounts.length === 0) {
+        return <div>No accounts available</div>;
+    }
+
+    return (
+        <Select value={selectedAccountId} onValueChange={setSelectedAccountId}>
+            <SelectTrigger className="w-full flex items-center text-left justify-center min-h-[8rem] border-0  shadow-[0_10px_25px_rgba(0,0,0,0.1)] rounded-lg">
+                {selectedAccountId ? (
+                    <div className="flex flex-col gap-1 ">
+                        <div className="text-sm font-semibold">ХАРИЛЦАХ / ИРГЭД / MNT</div>
+                        <div className="flex justify-between w-[650px]">
+                            <span className="text-sm font-semibold text-gray-500 block">
+                                MN{" "}
+                                {
+                                    accounts.find((acc) => acc.id === selectedAccountId)
+                                        ?.accountNumber
+                                }
+                            </span>
+                            <span className="text-sm font-semibold text-gray-500 block">
+                                {formatNumber(
+                                    accounts.find((acc) => acc.id === selectedAccountId)
+                                        ?.balance || 0
+                                )}{" "}
+                                MNT
+                            </span>
+                        </div>
+                    </div>
+                ) : (
+                    <span className="text-gray-400">Данс сонгоно уу</span>
+                )}
+            </SelectTrigger>
+
+            <SelectContent className="max-h-[500px]">
+                <SelectGroup>
+                    <SelectLabel className="ml-5.5 uppercase">Данс</SelectLabel>
+                    {accounts.map((account) => (
+                        <SelectItem
+                            className="h-16 py-4 flex justify-center"
+                            key={account.id}
+                            value={account.id}
+                        >
+                            <div className="flex flex-col gap-2">
+                                <div className="text-sm font-semibold">ХАРИЛЦАХ/PINE</div>
+                                <div className="flex justify-between w-[650px]">
+                                    <span className="text-sm font-semibold text-gray-500 block">
+                                        {account.accountNumber}
+                                    </span>
+                                    <span className="text-sm font-semibold text-gray-500 block">
+                                        {formatNumber(account?.balance || 0)} MNT
+                                    </span>
+                                </div>
+                            </div>
+                        </SelectItem>
+                    ))}
+                </SelectGroup>
+            </SelectContent>
+        </Select>
+    );
+};
+
+export default AccountSelector;
