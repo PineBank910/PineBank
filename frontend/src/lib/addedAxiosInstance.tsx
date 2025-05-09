9e2fd7c6b8a1e0d7fa21c579d15f8fa45bae30fa
24LP2417 24LP2417 <24LP2417@G301-CC0203-2.local>
2025-05-09 11:33:34 +0800

    FRONTEND: fix errors

--- frontend/src/lib/addedAxiosInstance.tsx
+++ frontend/src/lib/addedAxiosInstance.tsx
@@ -1,6 +1,6 @@
 
 import axios from "axios";
-export const axiosInstance = axios.create({ baseURL: "http://localhost:8000" });
+export const axiosInstance = axios.create({ baseURL: "https://pinebank.onrender.com" });
 
 // https://pinebank.onrender.com
 // http://localhost:8000
