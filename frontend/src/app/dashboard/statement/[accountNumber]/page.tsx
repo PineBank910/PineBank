23c01dd055a9af6adca2042387a778ef3803c07c
Juu316 <uujuu316@gmail.com>
2025-05-13 00:20:47 +0800

    FRONTEND: Transaction pass added

--- frontend/src/app/dashboard/statement/[accountNumber]/page.tsx
+++ frontend/src/app/dashboard/statement/[accountNumber]/page.tsx
@@ -14,7 +14,7 @@ import { addDays, endOfDay, startOfDay, subDays } from "date-fns";
 import ChooseAccountWithId from "./_components/ChooseAccountWithId";
 import { DatePickerWithRange } from "./_components/filterDate";
 import { Skeleton } from "@/components/ui/skeleton";
-import { downloadPDF } from "./_components/DownloadPDF";
+import { downloadPDF } from "./_components/downloadPDF";
 
 type Account = {
   accountNumber: string;
@@ -128,20 +128,19 @@ const Page = () => {
           selectedAccountId={selectedAccountId}
           setSelectedAccountId={setSelectedAccountId}
         />
-        
+
         <Button
           onClick={() => {
             if (account) {
               downloadPDF(filteredTransactions);
             }
           }}
-          className="w-12 h-12 rounded-lg"
-        >
+          className="w-12 h-12 rounded-lg">
           PDF
         </Button>
       </div>
       <div className="flex items-center justify-between p-4 mt-4 bg-secondary rounded-2xl">
-        <DatePickerWithRange date={dateRange} setDate={setDateRange}/>
+        <DatePickerWithRange date={dateRange} setDate={setDateRange} />
         <div className="flex gap-2">
           <Button onClick={setYesterday}>Өчигдөр</Button>
           <Button onClick={setLast7Days}>7 хоног</Button>
@@ -150,32 +149,29 @@ const Page = () => {
       </div>
       <div className="flex items-center justify-between w-full p-4 mt-4 rounded-2xl bg-secondary">
         <div className="flex gap-2">
-          <p className="flex flex-col items-end p-2 text-xl text-white bg-black  dark:text-black dark:bg-white rounded-xl">
+          <div className="flex flex-col items-end p-2 text-xl text-white bg-black  dark:text-black dark:bg-white rounded-xl">
             Нийт орлого:
             <div className="">{totalIncome}₮</div>
-          </p>
-          <p className="flex flex-col items-end p-2 text-xl text-white bg-black  dark:text-black dark:bg-white rounded-xl">
+          </div>
+          <div className="flex flex-col items-end p-2 text-xl text-white bg-black  dark:text-black dark:bg-white rounded-xl">
             Нийт зарлага:
             <div className="">{totalOutcome}₮</div>
-          </p>
+          </div>
         </div>
         <div className="flex gap-2">
           <Button
             className={filter === "ALL" ? "bg-green-500 flex" : " flex"}
-            onClick={() => setFilter("ALL")}
-          >
+            onClick={() => setFilter("ALL")}>
             Бүгд
           </Button>
           <Button
             className={filter === "CREDIT" ? "bg-green-500 flex" : " flex"}
-            onClick={() => setFilter("CREDIT")}
-          >
+            onClick={() => setFilter("CREDIT")}>
             Орлого
           </Button>
           <Button
             className={filter === "DEBIT" ? "bg-green-500 flex" : " flex"}
-            onClick={() => setFilter("DEBIT")}
-          >
+            onClick={() => setFilter("DEBIT")}>
             Зарлага
           </Button>
         </div>
@@ -185,8 +181,7 @@ const Page = () => {
           {[1, 2, 3].map((_, idx) => (
             <div
               key={idx}
-              className="p-4 space-y-2 border border-gray-200 rounded-lg"
-            >
+              className="p-4 space-y-2 border border-gray-200 rounded-lg">
               <Skeleton className="w-24 h-5" />
               {[1, 2].map((i) => (
                 <div key={i} className="flex items-center justify-between">
@@ -208,16 +203,12 @@ const Page = () => {
       {!loading && Object.keys(groupedTransactions).length > 0 && (
         <div className="rounded-2xl mt-4 w-full h-[700px] overflow-y-auto border">
           {Object.keys(groupedTransactions).map((date) => (
-            <div
-              key={date}
-              className="p-4 mb-4"
-            >
+            <div key={date} className="p-4 mb-4">
               <h3 className="text-xl font-semibold">{date}</h3>
               {groupedTransactions[date].map((transaction) => (
                 <div
                   key={transaction.id}
-                  className="flex items-center justify-between p-4 border-b"
-                >
+                  className="flex items-center justify-between p-4 border-b">
                   <div className="flex flex-col items-start justify-between">
                     <div className="text-sm text-gray-500">
                       {new Date(transaction.timestamp).toLocaleTimeString([], {
@@ -235,8 +226,7 @@ const Page = () => {
                         transaction.type === "DEBIT"
                           ? "text-red-500 flex"
                           : "text-green-500 flex"
-                      }
-                    >
+                      }>
                       {transaction.type === "DEBIT" ? "-" : "+"}
                       {isVisible ? (
                         <div className="font-medium">{transaction.amount}₮</div>
