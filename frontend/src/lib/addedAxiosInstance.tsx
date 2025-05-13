c51bb3e408fcc5a291194f29615466942e161825
Usukh-Erdene Choijamts <uujuu316@gmail.com>
2025-05-13 19:47:38 +0800

    Update addedAxiosInstance.tsx

--- frontend/src/lib/addedAxiosInstance.tsx
+++ frontend/src/lib/addedAxiosInstance.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
-  baseURL: "https://pinebank.onrender"});
+  baseURL: "https://pinebank.onrender.com"});
 // http://localhost:8000 when testing
 // https://pinebank.onrender.com when deployed
