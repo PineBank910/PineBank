8ac5d9db21e7b3f9c41e9c5060c6e144eb4f805e
24LP0058 24LP0058 <24LP0058@G301-CC0203.local>
2025-05-15 14:13:03 +0800

    FRONTEND: Mdkue sd baahan l ym uurchilluu

--- frontend/src/app/dashboard/transfer/_components/ChooseAccount.tsx
+++ frontend/src/app/dashboard/transfer/_components/ChooseAccount.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect } from "react";
-
+import { useVisibility } from "@/context/visibilityContext";
 import {
   Select,
   SelectContent,
@@ -15,6 +15,7 @@ type ChooseAccountProps = {
   setSelectedAccountId: (accountId: string) => void;
 };
 const ChooseAccount = (props: ChooseAccountProps) => {
+  const { isVisible } = useVisibility();
   const context = useContext(CurrentUser);
   const currentUserData = context?.currentUserData;
   const { selectedAccountId, setSelectedAccountId } = props;
@@ -45,12 +46,14 @@ const ChooseAccount = (props: ChooseAccountProps) => {
 
   return (
     <Select value={selectedAccountId} onValueChange={setSelectedAccountId}>
-      <SelectTrigger className="w-full flex items-center text-left justify-center min-h-16  border-0 shadow-[0_10px_25px_rgba(0,0,0,0.1)] rounded-lg">
+      <SelectTrigger className="w-full flex items-center text-left justify-center min-h-16 border cursor-pointer  shadow-2xs rounded-lg">
         {selectedAccountId ? (
-          <div className="flex justify-between items-center w-full">
-            <div className="flex flex-col justify-between w-full">
-              <div className="text-sm font-semibold">ХАРИЛЦАХ/PINE</div>
-              <span className="text-sm font-semibold text-gray-500 block">
+          <div className="flex justify-between items-center gap-1 w-full">
+            <div className="flex flex-col">
+              <div className="text-[10px]  sm:text-sm font-semibold">
+                ХАРИЛЦАХ / PINE / MNT
+              </div>
+              <span className="text-[10px] sm:text-sm font-semibold text-gray-500 block">
                 MN{" "}
                 {
                   accounts.find((acc) => acc.id === selectedAccountId)
@@ -58,44 +61,124 @@ const ChooseAccount = (props: ChooseAccountProps) => {
                 }
               </span>
             </div>
-            <span className="text-sm font-semibold text-gray-500 block">
-              {formatNumber(
-                Number(
-                  accounts.find((acc) => acc.id === selectedAccountId)?.balance
-                ) || 0
-              )}{" "}
-              MNT
-            </span>
+            {isVisible ? (
+              <span className="text-[10px] sm:text-sm font-semibold text-gray-500 block">
+                {formatNumber(
+                  Number(
+                    accounts.find((acc) => acc.id === selectedAccountId)
+                      ?.balance
+                  ) || 0
+                )}{" "}
+                MNT
+              </span>
+            ) : (
+              <div className="text-lg tracking-widest select-none">******</div>
+            )}
           </div>
         ) : (
           <span className="text-gray-400">Данс сонгоно уу</span>
         )}
       </SelectTrigger>
 
-      <SelectContent className="max-h-[500px]">
-        <SelectGroup>
-          <SelectLabel className="ml-5.5 uppercase">Данс</SelectLabel>
+      <SelectContent className="max-h-[500px] w-full">
+        <SelectGroup className="w-full">
+          <SelectLabel className="ml-5.5 uppercase ">Данс</SelectLabel>
           {accounts.map((account) => (
             <SelectItem
-              className="h-16 py-4 flex justify-center"
+              className="w-full p-2 block cursor-pointer"
               key={account.id}
               value={account.id}>
-              <div className="flex flex-col gap-2">
-                <div className="text-sm font-semibold">ХАРИЛЦАХ/PINE</div>
-                <div className="flex justify-between w-[650px]">
-                  <span className="text-sm font-semibold text-gray-500 block">
+              <div className="flex justify-between items-center w-full mr-6">
+                <div className="flex flex-col text-[10px] sm:text-sm">
+                  <div className="text-[10px] sm:text-sm font-semibold">
+                    ХАРИЛЦАХ / PINE / MNT
+                  </div>
+                  <span className="text-[10px] sm:text-sm font-semibold text-gray-500 block">
                     {account.accountNumber}
                   </span>
-                  <span className="text-sm font-semibold text-gray-500 block">
-                    {formatNumber(Number(account?.balance || 0))} MNT
-                  </span>
                 </div>
+                {isVisible ? (
+                  <span className="text-[10px] sm:text-sm font-semibold text-gray-500 block">
+                    {formatNumber(Number(account.balance))} MNT
+                  </span>
+                ) : (
+                  <div className="text-lg tracking-widest select-none">
+                    ******
+                  </div>
+                )}
               </div>
             </SelectItem>
           ))}
         </SelectGroup>
       </SelectContent>
     </Select>
+    // <Select value={selectedAccountId} onValueChange={setSelectedAccountId}>
+    //   <SelectTrigger className="w-full flex items-center text-left justify-center min-h-16 border-0 shadow-[0_10px_25px_rgba(0,0,0,0.1)] rounded-lg">
+    //     {selectedAccountId ? (
+    //       <div className="flex justify-between items-center w-full">
+    //         <div className="flex flex-col justify-between w-full">
+    //           <div className="text-sm font-semibold">ХАРИЛЦАХ / PINE / MNT</div>
+    //           <span className="text-sm font-semibold text-gray-500 block">
+    //             MN{" "}
+    //             {
+    //               accounts.find((acc) => acc.id === selectedAccountId)
+    //                 ?.accountNumber
+    //             }
+    //           </span>
+    //         </div>
+    //         {isVisible ? (
+    //           <span className="text-sm font-semibold text-gray-500 block">
+    //             {formatNumber(
+    //               Number(
+    //                 accounts.find((acc) => acc.id === selectedAccountId)
+    //                   ?.balance
+    //               ) || 0
+    //             )}{" "}
+    //             MNT
+    //           </span>
+    //         ) : (
+    //           <div className="text-lg tracking-widest select-none">******</div>
+    //         )}
+    //       </div>
+    //     ) : (
+    //       <span className="text-gray-400">Данс сонгоно уу</span>
+    //     )}
+    //   </SelectTrigger>
+
+    //   <SelectContent
+    //     style={{ width: "100%" }}
+    //     className="w-full min-w-0 border-0 shadow-[0_10px_25px_rgba(0,0,0,0.1)] rounded-lg">
+    //     <SelectGroup className="w-full">
+    //       <SelectLabel className=" uppercase">Данс</SelectLabel>
+    //       {accounts.map((account) => (
+    //         <SelectItem
+    //           className="h-16 py-4 w-full"
+    //           key={account.id}
+    //           value={account.id}>
+    //           <div className="w-full flex justify-between items-center">
+    //             <div>
+    //               <div className="text-sm font-semibold">
+    //                 ХАРИЛЦАХ / PINE / MNT
+    //               </div>
+    //               <span className="text-sm font-semibold text-gray-500 block">
+    //                 MN {account.accountNumber}
+    //               </span>
+    //             </div>
+    //             {isVisible ? (
+    //               <div className="text-sm font-semibold text-gray-500 block">
+    //                 {formatNumber(Number(account.balance) || 0)} MNT
+    //               </div>
+    //             ) : (
+    //               <div className="text-lg tracking-widest select-none">
+    //                 ******
+    //               </div>
+    //             )}
+    //           </div>
+    //         </SelectItem>
+    //       ))}
+    //     </SelectGroup>
+    //   </SelectContent>
+    // </Select>
   );
 };
 
